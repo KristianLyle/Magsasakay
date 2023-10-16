11cb1dcdb5d03929a987b942833b15f9120da1b4
Rainer Mayagma <rtmayagma@up.edu.ph>
2023-10-16 20:27:44 +0800

    Create App.js
    Map function

---
+++ magsasakay/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Maps from './Maps'; // Import your map component
+import './index.css';
+
+function App() {
+  return (
+    <main>
+      <Maps />
+    </main>
+  );
+}
+
+export default App;
