7a0eb3d1ddfdaeb0e96762f8c98be08c52a06f6b
Kurt Amodia <99860368+ainzzcutie@users.noreply.github.com>
2023-10-16 20:56:24 +0800

    Revert "Osm implementation"

--- magsasakay/src/App.js
+++ magsasakay/src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import Maps from './Maps'; // Import your map component
+import { BrowserRouter as Router, Route, Switch } from  'react-router-dom';
 import './index.css';
+import Login from './Login'
+import SignUp from './SignUp';
 
 function App() {
   return (
-    <main>
-      <Maps />
-    </main>
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <Login/>
+        </Route>
+        <Route exact path='/signUp'>
+          <SignUp/>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
