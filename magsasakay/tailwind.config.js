698cd235ec219d9f9b0bcc3002537969c080a8c2
KristianLyle <99728459+KristianLyle@users.noreply.github.com>
2023-10-21 00:37:35 +0800

    Added styles to navbar and home page
    Homepage design did not completely follow mockup.

--- magsasakay/tailwind.config.js
+++ magsasakay/tailwind.config.js
@@ -4,8 +4,16 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {backgroundImage: {'image1':"url('img/img1.jpg')",},},
-    fontFamily: {Montserrat: ['Montserrat']},
+    extend: {
+      backgroundImage: {
+        'image1': "url('img/img1.jpg')",
+        'home_bg': "url('img/home_bg.jpg')",
+        'wte_bg': "url('img/wte_bg.jpg')"
+      },
+    },
+    fontFamily: {
+      Montserrat: ['Montserrat'],
+    },
   },
   plugins: [],
-}
+};
