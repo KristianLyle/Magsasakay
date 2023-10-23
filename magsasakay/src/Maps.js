ff110b1987b5de337450bd312176dcd3dff07104
Kurt Amodia <kaamodia@up.edu.ph>
2023-10-23 15:08:04 +0800

    Update Maps.js

--- magsasakay/src/Maps.js
+++ magsasakay/src/Maps.js
@@ -1,11 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import { MapContainer, TileLayer, Polyline, Popup, Marker } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
-import { decodePolyline } from './PolylineDecoder';
-import routeInfo from './RouteInfo.json';
-import RouteDropdown from './RouteDropdown';
-import L from 'leaflet';
-import logo from './marker.png';
+import React, { useState, useEffect } from "react";
+import {
+  MapContainer,
+  TileLayer,
+  Polyline,
+  Popup,
+  Marker,
+} from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import { decodePolyline } from "./PolylineDecoder";
+import routeInfo from "./RouteInfo.json";
+import RouteDropdown from "./RouteDropdown";
+import L from "leaflet";
+import logo from "./marker.png";
+import v_bg from "./img/v-bg.mp4";
 
 const customIcon = L.icon({
   iconUrl: logo,
@@ -15,7 +22,7 @@ const customIcon = L.icon({
 });
 
 const MapComponent = () => {
-  const [selectedRouteIndex, setSelectedRouteIndex] = useState('');
+  const [selectedRouteIndex, setSelectedRouteIndex] = useState("");
   const routes = routeInfo;
   const selectedRoute = routes[selectedRouteIndex];
   const [route, setRoute] = useState(null);
@@ -28,11 +35,15 @@ const MapComponent = () => {
       return;
     }
 
-    const osrmCoordinates = selectedRoute.coordinates.map((coord) => `${coord.lon},${coord.lat}`).join(';');
+    const osrmCoordinates = selectedRoute.coordinates
+      .map((coord) => `${coord.lon},${coord.lat}`)
+      .join(";");
 
     const fetchRoute = async () => {
       try {
-        const response = await fetch(`http://router.project-osrm.org/route/v1/driving/${osrmCoordinates}`);
+        const response = await fetch(
+          `http://router.project-osrm.org/route/v1/driving/${osrmCoordinates}`
+        );
 
         if (response.ok) {
           const data = await response.json();
@@ -44,10 +55,10 @@ const MapComponent = () => {
             setDecodedGeometry(decodedCoordinates);
           }
         } else {
-          console.error('Failed to fetch route data');
+          console.error("Failed to fetch route data");
         }
       } catch (error) {
-        console.error('Error fetching route data', error);
+        console.error("Error fetching route data", error);
       }
     };
 
@@ -55,49 +66,72 @@ const MapComponent = () => {
   }, [selectedRoute]);
 
   return (
-    <div>
-      <div>
-        <RouteDropdown
-          routes={routes}
-          selectedRoute={selectedRouteIndex}
-          onSelectRoute={setSelectedRouteIndex}
+    <>
+      <div className="relative h-screen">
+        <video
+          src={v_bg}
+          autoPlay
+          loop
+          muted
+          className="absolute w-full h-full object-cover z-0"
         />
+        <div className="absolute w-full h-full bg-gradient-to-t from-indigo-500 to-orange-500 z-5 opacity-60"></div>
+        <div className="relative z-10">
+          <div className="text-center ml-[30px]">
+            <RouteDropdown
+              routes={routes}
+              selectedRoute={selectedRouteIndex}
+              onSelectRoute={setSelectedRouteIndex}
+            />{" "}
+            <br />
+          </div>
+          <div className="items-center">
+            <MapContainer
+              center={[10.7202, 122.5621]}
+              zoom={13}
+              className="rounded-[30px] h-[450px] w-[700px] ml-[415px] shadow-lg shadow-black-500/40
+                      border-[5px] border-[#160E3D]"
+            >
+              <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              />
+              {route && (
+                <div className="items-center">
+                  <Polyline
+                    positions={decodedGeometry}
+                    color="blue"
+                    weight={5}
+                    opacity={0.7}
+                  >
+                    <Popup>
+                      <div className="items">
+                        <h3>{selectedRoute.title}</h3>
+                        <p>{selectedRoute.description}</p>
+                      </div>
+                    </Popup>
+                  </Polyline>
+                  {selectedRoute.establishments.map((establishment, index) => (
+                    <Marker
+                      key={index}
+                      position={[establishment.lat, establishment.lon]}
+                      icon={customIcon}
+                    >
+                      <Popup>
+                        <div>
+                          <h3>{establishment.name}</h3>
+                          <p>Type: {establishment.type}</p>
+                        </div>
+                      </Popup>
+                    </Marker>
+                  ))}
+                </div>
+              )}
+            </MapContainer>
+          </div>
+        </div>
       </div>
-      <div>
-        <MapContainer center={[10.7202, 122.5621]} zoom={13} style={{ height: '500px', width: '500px' }}>
-          <TileLayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          />
-          {route && (
-            <div>
-              <Polyline positions={decodedGeometry} color="blue" weight={5} opacity={0.7}>
-                <Popup>
-                  <div>
-                    <h3>{selectedRoute.title}</h3>
-                    <p>{selectedRoute.description}</p>
-                  </div>
-                </Popup>
-              </Polyline>
-              {selectedRoute.establishments.map((establishment, index) => (
-                <Marker
-                  key={index}
-                  position={[establishment.lat, establishment.lon]}
-                  icon={customIcon}
-                >
-                  <Popup>
-                    <div>
-                      <h3>{establishment.name}</h3>
-                      <p>Type: {establishment.type}</p>
-                    </div>
-                  </Popup>
-                </Marker>
-              ))}
-            </div>
-          )}
-        </MapContainer>
-      </div>
-    </div>
+    </>
   );
 };
 
