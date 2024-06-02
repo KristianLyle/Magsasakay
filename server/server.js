0e1640c1bd9de0a18edb507632751e92f89373d2
Kurt Amodia <kaamodia@up.edu.ph>
2024-06-02 23:26:47 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -495,6 +495,33 @@ app.post("/update-user-info", async (req, res) => {
       return res.status(404).json({ error: "Profile not found" });
     }
 
+    if (username) {
+      // Update the username in all reviews by the user
+      await reviewModel.updateMany(
+        { useremail: currentEmail },
+        { $set: { username: username } }
+      );
+    }
+
+    // Get all the restaurants where the user had reviews
+    const restaurantsWithUserReviews = await reviewModel
+      .find({ useremail: email })
+      .distinct("restaurant");
+
+    // Update the average ratings for each restaurant
+    for (const restaurantName of restaurantsWithUserReviews) {
+      const reviews = await reviewModel.find({ restaurant: restaurantName });
+      const totalRatings = reviews.reduce((acc, curr) => acc + curr.rating, 0);
+      const numReviews = reviews.length;
+      const averageRating = numReviews ? totalRatings / numReviews : 0; // Check if numReviews is not zero before calculating average
+
+      // Update the averageRating of the specific restaurant
+      await restaurantModel.updateOne(
+        { name: restaurantName },
+        { averageRating }
+      );
+    }
+
     res
       .status(201)
       .json({ message: "Profile updated successfully.", updatedProfile });
@@ -540,6 +567,14 @@ app.post("/upload-image", upload.single("image"), async (req, res) => {
 
     if (!updatedProfile) {
       return res.status(404).json({ error: "User not found" });
+    }
+
+    if (email) {
+      // Update the userimage in all reviews by the user
+      await reviewModel.updateMany(
+        { useremail: email },
+        { $set: { userimage: imageName } }
+      );
     }
 
     res.json({ status: "ok" });
